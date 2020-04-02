a63138caa41052b5c44428b75ea14d5e2b660ac2
Connor Reese <connor.reese13@gmail.com>
2020-04-02 11:47:36 -0600

    added cards for projects section

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,90 @@
 import React from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardText,
+  CardTitle,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton
+} from "react-mdl";
 
 class Projects extends React.Component {
+  state = {
+    activeTab: 0
+  };
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#FFF",
+                height: "176px",
+                background:
+                  "url(https://img.stackshare.io/service/1020/OYIaJ1KK.png) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It
+            </CardText>
+            <CardActions border>
+              <Button className="colored">GitHub</Button>
+              <Button className="colored">CodePen</Button>
+              <Button className="colored">Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is MongoDB</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1>This is Test Automation</h1>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Projects Page</h1>
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>MongoDB</Tab>
+          <Tab>Test Automation</Tab>
+        </Tabs>
+
+        <section className="projects-grid">
+          <Grid className="projects-grid">
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+        </section>
       </div>
     );
   }
