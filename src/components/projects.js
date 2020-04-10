072ee74d0952705983bec69577b51b916c6d25a3
Connor Reese <connor.reese13@gmail.com>
2020-04-10 11:11:47 -0600

    added styling and more projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -30,14 +30,15 @@ class Projects extends React.Component {
                 background:
                   "url(https://www.abc.net.au/news/image/9627246-3x2-340x227.jpg) center / cover"
               }}
-            >
-              Selenium & Cucumber BDD
-            </CardTitle>
+            ></CardTitle>
             <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It
+              <strong style={{ fontSize: "1.3rem" }}>
+                Selenium & Cucumber BDD
+              </strong>
+              <br />
+              <br />
+              Testing framework utilizing Cucumbe Behavior Driven Development &
+              Gherkin syntax. Writen in Ruby.
             </CardText>
             <CardActions border style={{ textAlign: "center" }}>
               <a
@@ -47,9 +48,7 @@ class Projects extends React.Component {
                 <Button className="colored">GitHub</Button>
               </a>
             </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
+            <CardMenu style={{ color: "#fff" }}></CardMenu>
           </Card>
 
           <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
@@ -58,25 +57,27 @@ class Projects extends React.Component {
                 color: "#FFF",
                 height: "176px",
                 background:
-                  "url(https://img.stackshare.io/service/1020/OYIaJ1KK.png) center / cover"
+                  "url(https://pbs.twimg.com/profile_images/993969044674953216/Peyxjyb2_400x400.jpg) center / cover"
               }}
-            >
-              React Project #3
-            </CardTitle>
+            ></CardTitle>
             <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It
+              <strong style={{ fontSize: "1.3rem" }}>
+                Selenium & Nightwatch.js
+              </strong>
+              <br />
+              <br />
+              Test Automation framekwork using Nigthwatch.js, written in
+              Javascript.
             </CardText>
-            <CardActions border>
-              <Button className="colored">GitHub</Button>
-              <Button className="colored">CodePen</Button>
-              <Button className="colored">Live Demo</Button>
+            <CardActions border style={{ textAlign: "center" }}>
+              <a
+                href="https://github.com/connorreese13/connor-el-sistema-automation"
+                target="_blank"
+              >
+                <Button className="colored">GitHub</Button>
+              </a>
             </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
+            <CardMenu style={{ color: "#fff" }}></CardMenu>
           </Card>
         </div>
       );
@@ -91,14 +92,14 @@ class Projects extends React.Component {
                 background:
                   "url(https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80) center / cover"
               }}
-            >
-              AirBnb Clone
-            </CardTitle>
+            ></CardTitle>
             <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It
+              <strong style={{ fontSize: "1.3rem" }}>AirBnb Clone</strong>
+              <br />
+              <br />
+              AirBnB clone making use of multiple components, pages, and
+              collections and utilizing client-side APIs and third-party
+              components
             </CardText>
             <CardActions border style={{ textAlign: "center" }}>
               <a
@@ -114,9 +115,7 @@ class Projects extends React.Component {
                 <Button className="colored">Live Demo</Button>
               </a>
             </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
+            <CardMenu style={{ color: "#fff" }}></CardMenu>
           </Card>
 
           <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
@@ -127,14 +126,11 @@ class Projects extends React.Component {
                 background:
                   "url(https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80) center / cover"
               }}
-            >
-              To Do List
-            </CardTitle>
+            ></CardTitle>
             <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It
+              <strong style={{ fontSize: "1.3rem" }}> To Do List</strong>
+              <br />
+              <br />A simple but elegant todo CRUD application
             </CardText>
             <CardActions border style={{ textAlign: "center" }}>
               <a
@@ -150,9 +146,7 @@ class Projects extends React.Component {
                 <Button className="colored">Live Demo</Button>
               </a>
             </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
+            <CardMenu style={{ color: "#fff" }}></CardMenu>
           </Card>
 
           <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
@@ -161,25 +155,31 @@ class Projects extends React.Component {
                 color: "#FFF",
                 height: "176px",
                 background:
-                  "url(https://img.stackshare.io/service/1020/OYIaJ1KK.png) center / cover"
+                  "url(https://images.unsplash.com/photo-1512428559087-560fa5ceab42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80) center / cover"
               }}
-            >
-              React Project #3
-            </CardTitle>
+            ></CardTitle>
             <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It
+              <strong style={{ fontSize: "1.3rem" }}> Chat App</strong>
+              <br />
+              <br />
+              Chat app similar to slack requiring user authentication for login
+              and sign up
             </CardText>
-            <CardActions border>
-              <Button className="colored">GitHub</Button>
-              <Button className="colored">CodePen</Button>
-              <Button className="colored">Live Demo</Button>
+            <CardActions border style={{ textAlign: "center" }}>
+              <a
+                href="https://github.com/connorreese13/06-slack-client"
+                target="_blank"
+              >
+                <Button className="colored">GitHub</Button>
+              </a>
+              <a
+                href="https://connor-slack-clone-client.herokuapp.com/"
+                target="_blank"
+              >
+                <Button className="colored">Live Demo</Button>
+              </a>
             </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
+            <CardMenu style={{ color: "#fff" }}></CardMenu>
           </Card>
         </div>
       );
